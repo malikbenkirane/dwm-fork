1538e486f1886c6b16b138b9c7525738b3b77d3a
Malik BEN KIRANE <free.malikbenkirane@gmail.com>
2018-04-18 02:11:54 +0200

    Patch nametag and new keybindings - nametag patch manually applied - MODKEY_v binded to view() - MODKEY_Tab binded to focusstack (+1) or next client - MODKEY_n for nametag

--- config.def.h
+++ config.def.h
@@ -16,7 +16,10 @@ static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 0;        /* 0 means bottom bar */
 
 /* tagging */
-static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
+#define MAX_TAGNAME_LEN 14		/* excludes TAG_PREPEND */
+#define TAG_PREPEND "%1i:"		/* formatted as 2 chars */
+#define MAX_TAGLEN 16			/* altogether */
+static char tags[][MAX_TAGLEN] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
 
 static const Rule rules[] = {
 	/* xprop(1):
@@ -30,7 +33,7 @@ static const Rule rules[] = {
 
 /* layout(s) */
 static const float mfact     = 0.5; /* factor of master area size [0.05..0.95] */
-static const int nmaster     = 2;    /* number of clients in master area */
+static const int nmaster     = 1;    /* number of clients in master area */
 static const int resizehints = 0;    /* 1 means respect size hints in tiled resizals */
 
 #include "grid.c"
@@ -54,7 +57,7 @@ static const Layout layouts[] = {
 #define SHCMD(cmd) { .v = (const char*[]){ "/bin/sh", "-c", cmd, NULL } }
 
 /* commands */
-static const char *termcmd[]  = { "urxvt", NULL };
+static const char *termcmd[]  = { "st", NULL };
 static const char *roficmd[] = { "/home/void/.config/myrofi", "run", NULL };
 
 static Key keys[] = {
@@ -63,13 +66,14 @@ static Key keys[] = {
 	{ MODKEY|ShiftMask,                   XK_Return, spawn,          {.v = termcmd } },
 	{ MODKEY,                             XK_b,      togglebar,      {0} },
 	{ MODKEY,                             XK_j,      focusstack,     {.i = +1 } },
+        { MODKEY,                             XK_Tab,    focusstack,     {.i = +1 } },
 	{ MODKEY,                             XK_k,      focusstack,     {.i = -1 } },
 	{ MODKEY,                             XK_i,      incnmaster,     {.i = +1 } },
 	{ MODKEY,                             XK_d,      incnmaster,     {.i = -1 } },
 	{ MODKEY,                             XK_h,      setmfact,       {.f = -0.05} },
 	{ MODKEY,                             XK_l,      setmfact,       {.f = +0.05} },
 	{ MODKEY,                             XK_Return, zoom,           {0} },
-	{ MODKEY,                             XK_Tab,    view,           {0} },
+	{ MODKEY,                             XK_v,      view,           {0} },
 	{ MODKEY|ShiftMask,                   XK_w,      killclient,     {0} },
 	{ MODKEY,                             XK_t,      setlayout,      {.v = &layouts[0]} },
 	{ MODKEY,                             XK_m,      setlayout,      {.v = &layouts[1]} },
@@ -94,6 +98,7 @@ static Key keys[] = {
 	TAGKEYS(                              XK_9,                      8)
 	{ MODKEY|ShiftMask,                   XK_q,      quit,           {0} },
         { MODKEY,                             XK_o,      winview,        {0} },
+        { MODKEY,                             XK_n,      nametag,        {0} },
 };
 
 /* button definitions */
